0eff2e7a06029cb4b1084054f767dbe6daf4d46a
gaogeli <gyli@embed-soft.cn>
2019-11-05 04:35:56 +0800

    convert to spring boot 2.2.0

---
+++ ecp-uid-springboot/src/main/java/com/myzmds/ecp/boot/EcpUidSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.myzmds.ecp.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcpUidSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcpUidSpringbootApplication.class, args);
+    }
+
+}
---
+++ ecp-uid-springboot/src/test/java/com/myzmds/ecp/boot/EcpUidSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myzmds.ecp.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcpUidSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
